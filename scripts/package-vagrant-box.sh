295f39ef5749f4051462ab3bde4aa8e29f22f679
JP Toto <jtoto@emoneyadvisor.com>
2018-05-24 09:59:11 -0400

    minor hardware experiments

--- scripts/package-vagrant-box.sh
+++ scripts/package-vagrant-box.sh
@@ -14,11 +14,13 @@ box="$2"
 vmname="$(basename "${vmdk}" .vmdk)"
 
 # Create VirtualBox vm
-VBoxManage createvm --name "${vmname}" --ostype RedHat_64 --register
+VBoxManage createvm --name "${vmname}" --ostype Linux_64 --register
 
 # Configure vmdk disk
-VBoxManage storagectl "${vmname}" --name SATA --add sata --controller IntelAhci
+VBoxManage storagectl "${vmname}" --name IDE --add ide --controller PIIX4 --portcount 2
+VBoxManage storagectl "${vmname}" --name SATA --add sata --controller IntelAhci --portcount 30
 VBoxManage storageattach "${vmname}" --storagectl SATA --port 0 --device 0 --type hdd --medium "${vmdk}"
+vboxmanage storageattach "${vmname}" --storagectl IDE --port 1 --device 0 --type dvddrive --medium emptydrive
 
 # Configure network drivers as virtio
 for i in 1 2 3 4; do
